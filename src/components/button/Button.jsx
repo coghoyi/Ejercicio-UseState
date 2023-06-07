d6d4cabc092d2e24544f0c7f0da0e4e446c642c7
coghoyi <davidhoyolopez@alumnostrazos.net>
2023-06-07 16:36:58 +0200

    Ejercicio-UseState

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Button = ()=>{
+    const [counter, setCounter] = useState(0);
+
+  const sumar = () => {
+    setCounter(counter + 1);
+  };
+
+  const restar = () => {
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={sumar}>Sumar</button> 
+      <button onClick={reset}>Reset</button>
+      <button onClick={restar}>Restar</button>
+     
+    </div>
+  );
+}
+
+export default Button;
