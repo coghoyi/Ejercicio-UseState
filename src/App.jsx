83dc857d4647f68fe400aa5580a20391fde38d91
coghoyi <davidhoyolopez@alumnostrazos.net>
2023-06-07 16:29:04 +0200

    Ejercicio-UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+  const [counter, setCounter] = useState(0);
+
+  const sumar = () => {
+    setCounter(counter + 1);
+  };
+
+  const restar = () => {
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={sumar}>Sumar</button> 
+      <button onClick={reset}>Reset</button>
+      <button onClick={restar}>Restar</button>
+     
+    </div>
+  );
 };
 
 export default App;
+
